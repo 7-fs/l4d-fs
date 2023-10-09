58d888128e42d323ef09fc9fc049a3809555abfd
MiLO-Src <mkdeosm@gmail.com>
2023-10-10 00:17:26 +0200

    win build
    not info

---
+++ start l4d.bat
@@ -0,0 +1,16 @@
+@echo off
+cls 
+color a
+title Left 4 Dead - MiLO_Src/xModea
+echo     ----------------------------------------
+echo             Powered by MiLO_Src/xModea
+echo     ----------------------------------------
+echo                    Left 4 Dead
+echo     ----------------------------------------
+echo                     Launching
+echo     ----------------------------------------
+echo                -language russian
+echo     ----------------------------------------
+
+@echo off 
+left4dead.exe -language russian -novid
