14b1cbfbb21082ac56945588c91a350457584bce
MiLO-Src <xbublicator@gmail.com>
2023-10-10 15:47:22 +0200

    work update v3.3
    update from 7.fs repository

---
+++ bin/toolpresets/hl2/.batchcommander/set_upaxis_to_z.command
@@ -0,0 +1,4 @@
+import assets
+xm = assets.ExportManager()
+for asset in xm:
+	asset.upAxis='Z'
--- start l4d.bat
+++ start l4d.bat
@@ -1,9 +1,9 @@
 @echo off
 cls 
 color a
-title Left 4 Dead - MiLO_Src/xModea
+title Left 4 Dead - Cleared Edition
 echo     ----------------------------------------
-echo             Powered by MiLO_Src/xModea
+echo             Powered by 7.fs
 echo     ----------------------------------------
 echo                    Left 4 Dead
 echo     ----------------------------------------
