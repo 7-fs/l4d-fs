b591f8305eb00bbddacd22a833d355165bf4fa61
MiLO-Src <xbublicator@gmail.com>
2023-10-14 17:03:49 +0200

    Update start l4d.bat

--- start l4d.bat
+++ start l4d.bat
@@ -1,16 +1,1 @@
-@echo off
-cls 
-color a
-title Left 4 Dead - Cleared Edition
-echo     ----------------------------------------
-echo             Powered by 7.fs
-echo     ----------------------------------------
-echo                    Left 4 Dead
-echo     ----------------------------------------
-echo                     Launching
-echo     ----------------------------------------
-echo                -language russian
-echo     ----------------------------------------
-
-@echo off 
-left4dead.exe -language russian -novid
+start left4dead.exe -language russian -novid
